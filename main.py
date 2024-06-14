b92f93ff5c78be6f5c3b6f8ebbfa2909c9306651
Miguel A Arenas <miguel.arenas@sitidoctor.com>
2024-06-14 15:26:46 -0500

    Refactor main.py to improve data decoding functionality

--- main.py
+++ main.py
@@ -85,3 +85,29 @@ def encode ():
 
   newimagename = input("Enter the name of new image(with extension): ")
   newimage.save(newimagename, str(newimagename.split(".")[1].upper()))
+
+# Decode the data in the image
+def decode():
+  img = input("Enter image name(with extension): ")
+  image = Image.open(img, 'r')
+
+  data = ''
+  imgdata = iter(image.getdata())
+
+  while (True):
+    pixels = [value for value in imgdata.__next__()[:3] +
+              imgdata.__next__()[:3] +
+              imgdata.__next__()[:3]]
+    
+    # string of binary data
+    binstr = ''
+
+    for i in pixels[:8]:
+      if (i % 2 == 0):
+        binstr += '0'
+      else:
+        binstr += '1'
+    
+    data += chr(int(binstr, 2))
+    if (pixels[-1] % 2 != 0):
+      return data
